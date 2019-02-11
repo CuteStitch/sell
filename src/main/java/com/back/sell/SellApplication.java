b754c5e8acd45c9cee1074d25ff3abb330594d32
Stitch <1317004425@qq.com>
2019-02-11 14:18:15 +0800

    测试

--- src/main/java/com/back/sell/SellApplication.java
+++ src/main/java/com/back/sell/SellApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SellApplication {
 
+    //项目启动位置
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
     }
